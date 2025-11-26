a6415d9cfa61c1d5e49beec2c4e15b08280a1761
Elena Romanova <romanova.bim@gmail.com>
2025-11-26 20:28:04 +0500

    добавлен лимит для минимального размера файлов

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -11,6 +11,13 @@ export const uploadFile = async (
         return next(new BadRequestError('Файл не загружен'))
     }
     try {
+        // Ограничение минимального размера файла - 2 KB
+        const MIN_SIZE = 2 * 1024
+        const fileSize = req.file.size ?? req.file.buffer?.length ?? 0;
+        if (fileSize < MIN_SIZE) {
+            return next(new BadRequestError('Файл меньше минимального допустимого размера - 2KB'))
+        }
+
         const fullPath = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
             : `/${req.file.filename}`
